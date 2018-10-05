c5a57e0241006c5566c606fa2f343846d0ccd417
Jae Choi <j.thechois@gmail.com>
2018-10-05 20:03:00 +0900

    Complete Search component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,55 @@
 class App extends React.Component {
   constructor() {
     super();
+    this.options = {
+      key: window.YOUTUBE_API_KEY,
+      query: '',
+      max: 6
+    };
+
     this.state = {
       currentVideo: window.exampleVideoData[0],
+      videoList: [],
     };
+
+    window.searchYouTube(this.options, videos => {
+      this.setState({
+        currentVideo: videos[0],
+        videoList: videos.slice(1)
+      });
+    });
   }
 
   changeVideo(video) {
     this.setState({currentVideo: video});
-  } 
+  }
+
+  // searchVideo(videos) {
+  //   this.setState({videoList: videos});
+  // }
+
+  searchVideo(event) {
+    this.options.query = event.target.value;
+    searchYouTube(this.options, data => {
+      this.setState({
+        currentVideo: data[0],
+        videoList: data.slice(1)
+      });
+    });
+  }
 
   render() {
     return (
       <div>
-        <Nav />
-        <div className="col-md-7">
-          <VideoPlayer video={this.state.currentVideo}/>
-        </div>
-        <div className="col-md-5">
-          <VideoList videos={window.exampleVideoData}
-            onClick={this.changeVideo.bind(this)}/>
+        <Nav search={this.searchVideo.bind(this)} />
+        <div>
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVideo}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videoList}
+              onClick={this.changeVideo.bind(this)}/>
+          </div>
         </div>
       </div>
     );
