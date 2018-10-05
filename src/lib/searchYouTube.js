2606acf5edc33b072bc0ee51db14dc6e021788b6
Jae Choi <j.thechois@gmail.com>
2018-10-05 16:36:54 +0900

    Complete searchYouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      'q': options.query,
+      'maxResults': options.max,
+      'key': options.key,
+      'part': 'snippet',
+      'type': 'video'
+    },
+    success: function(data) {
+      callback(data.items);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
